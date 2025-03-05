c276af8f26301f583edacf66478c52675e400364
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2025-03-05 04:53:38 -0800

    Create homepage

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,44 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 
-import "./App.module.css";
+import styles from "./App.module.css";
 
 function App() {
-  return <Outlet />;
+  return (
+    <>
+      <header className={styles.appHeader}>
+        <h1>Blogger</h1>
+        <div className={styles.navWrapper}>
+          <nav className={styles.appNav}>
+            <Link to="published">
+              <button type="button" className={styles.navBtn}>
+                Published
+              </button>
+            </Link>
+            <Link to="unpublished">
+              <button type="button" className={styles.navBtn}>
+                Unpublished
+              </button>
+            </Link>
+            <Link to="new-post">
+              <button type="button" className={styles.navBtn}>
+                New Post
+              </button>
+            </Link>
+            <Link to="log-out">
+              <button type="button" className={styles.navBtn}>
+                Log out
+              </button>
+            </Link>
+          </nav>
+        </div>
+      </header>
+      <main className={styles.appMain}>
+        <div className={styles.outletWrapper}>
+          <Outlet />
+        </div>
+      </main>
+    </>
+  );
 }
 
 export default App;

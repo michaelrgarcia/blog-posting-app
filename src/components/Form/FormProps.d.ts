53a0d08e8e7e566a9e301c1b28a74f56a0eae221
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2025-03-10 00:24:25 -0700

    Create Textarea component

--- src/components/Form/FormProps.d.ts
+++ src/components/Form/FormProps.d.ts
@@ -1,7 +1,10 @@
 import { InputProps } from "./FormComponents/Inputs/InputProps";
+import { TextareaProps } from "./FormComponents/Textarea/TextareaProps";
+
+export type FormFieldProps = InputProps | TextareaProps;
 
 export interface FormProps {
-  inputs: InputProps[];
+  inputs: FormFieldProps[];
   submitBtnTxt?: string;
   submitBtnColor?: string; // a valid CSS color property
   error: string | null;
